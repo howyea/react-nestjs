3a56c57255f38a73696cedfcf247c47a63be5a0e
Babel Ye <babel.hy.ye@hkjc.org.hk>
2022-11-09 21:26:59 +0800

    first commit

---
+++ react-cars-app/src/app/components/navbar/navItems.tsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import styled from 'styled-components'
+import tw from 'twin.macro'
+import { Slide as Menu } from 'react-burger-menu'
+import { useMediaQuery } from 'react-responsive'
+import { SCREENS } from '../responsive'
+
+const ListContainer = styled.ul`
+	${tw`
+        flex
+        list-none
+    `}
+`
+
+const NavItem = styled.li`
+	${tw`
+        text-xs
+        md:text-base
+        text-black
+        font-medium
+        mr-1
+        md:mr-5
+        cursor-pointer
+        transition
+        duration-300
+        ease-in-out
+        hover:text-gray-700
+    `}
+`
+
+export function NavItems() {
+	const isMobile = useMediaQuery({ maxWidth: SCREENS.sm })
+
+	if (isMobile) {
+		return (
+			<Menu>
+				<ListContainer>
+					<NavItem>
+						<a href='#'>Home</a>
+					</NavItem>
+					<NavItem>
+						<a href='#'>Cars</a>
+					</NavItem>
+					<NavItem>
+						<a href='#'>Services</a>
+					</NavItem>
+					<NavItem>
+						<a href='#'>Contact Us</a>
+					</NavItem>
+				</ListContainer>
+			</Menu>
+		)
+	}
+
+	return (
+		<ListContainer>
+			<NavItem>
+				<a href='#'>Home</a>
+			</NavItem>
+			<NavItem>
+				<a href='#'>Cars</a>
+			</NavItem>
+			<NavItem>
+				<a href='#'>Services</a>
+			</NavItem>
+			<NavItem>
+				<a href='#'>Contact Us</a>
+			</NavItem>
+		</ListContainer>
+	)
+}
