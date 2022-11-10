4876bd7538ff98917ecfaa3f844ddfeb699cd426
Babel Ye <babel.hy.ye@hkjc.org.hk>
2022-11-10 14:21:09 +0800

    add top section

--- react-cars-app/src/app/components/navbar/navItems.tsx
+++ react-cars-app/src/app/components/navbar/navItems.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import tw from 'twin.macro'
-import { Slide as Menu } from 'react-burger-menu'
+import { slide as Menu } from 'react-burger-menu'
 import { useMediaQuery } from 'react-responsive'
 import { SCREENS } from '../responsive'
-
+import menuStyles from './menuStyles'
 const ListContainer = styled.ul`
 	${tw`
         flex
@@ -12,9 +12,9 @@ const ListContainer = styled.ul`
     `}
 `
 
-const NavItem = styled.li`
+const NavItem = styled.li<{ menu?: any }>`
 	${tw`
-        text-xs
+        text-sm
         md:text-base
         text-black
         font-medium
@@ -25,7 +25,17 @@ const NavItem = styled.li`
         duration-300
         ease-in-out
         hover:text-gray-700
-    `}
+    `};
+	${({ menu }) =>
+		menu &&
+		css`
+			${tw`
+            text-white
+            text-xl
+            mb-3
+            focus:text-white
+        `};
+		`};
 `
 
 export function NavItems() {
@@ -33,18 +43,18 @@ export function NavItems() {
 
 	if (isMobile) {
 		return (
-			<Menu>
+			<Menu right styles={menuStyles}>
 				<ListContainer>
-					<NavItem>
+					<NavItem menu>
 						<a href='#'>Home</a>
 					</NavItem>
-					<NavItem>
+					<NavItem menu>
 						<a href='#'>Cars</a>
 					</NavItem>
-					<NavItem>
+					<NavItem menu>
 						<a href='#'>Services</a>
 					</NavItem>
-					<NavItem>
+					<NavItem menu>
 						<a href='#'>Contact Us</a>
 					</NavItem>
 				</ListContainer>
