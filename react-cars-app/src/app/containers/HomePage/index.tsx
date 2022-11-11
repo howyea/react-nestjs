c54dc5b9c77c1fbd1e0ea8642c5cdd787fd474b4
Babel Ye <babel.hy.ye@hkjc.org.hk>
2022-11-11 16:46:11 +0800

    add TopCars component

--- react-cars-app/src/app/containers/HomePage/index.tsx
+++ react-cars-app/src/app/containers/HomePage/index.tsx
@@ -6,6 +6,7 @@ import { Marginer } from '../../components/marginer'
 import { Navbar } from '../../components/navbar'
 import { AboutUs } from './aboutUs'
 import { BookingSteps } from './bookingSteps'
+import { TopCars } from './topCars'
 import { TopSection } from './topSection'
 const PageContainer = styled.div`
 	${tw`
@@ -25,11 +26,12 @@ export function HomePage() {
 			<TopSection />
 			<Marginer direction='vertical' margin='4em' />
 			<BookCard />
-			<Marginer direction='vertical' margin='4em' />
+			<Marginer direction='vertical' margin='10em' />
 			<BookingSteps />
 			<Marginer direction='vertical' margin='8em' />
 			<AboutUs />
 			<Marginer direction='vertical' margin='8em' />
+			<TopCars />
 		</PageContainer>
 	)
 }
