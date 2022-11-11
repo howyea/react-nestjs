0ed554ee95e802fa07ad06903611d5da88094058
Babel Ye <babel.hy.ye@hkjc.org.hk>
2022-11-11 20:54:22 +0800

    update TopCars component

--- react-cars-app/src/app/containers/HomePage/topCars.tsx
+++ react-cars-app/src/app/containers/HomePage/topCars.tsx
@@ -5,6 +5,8 @@ import { ICar } from '../../../typings/car'
 import { Car } from '../../components/car'
 import Carousel, { Dots, slidesToShowPlugin } from '@brainhubeu/react-carousel'
 import '@brainhubeu/react-carousel/lib/style.css'
+import { useMediaQuery } from 'react-responsive'
+import { SCREENS } from '../../components/responsive'
 
 const TopCarsContainer = styled.div`
 	${tw`
@@ -44,6 +46,7 @@ const CarsContainer = styled.div`
 
 export function TopCars() {
 	const [current, setCurrent] = useState(0)
+	const isMobile = useMediaQuery({ maxWidth: SCREENS.sm })
 	const testCar: ICar = {
 		name: 'Audi S3 Car',
 		mileage: '10k',
@@ -65,14 +68,56 @@ export function TopCars() {
 		gearType: 'Auto',
 		gas: 'Petrol',
 	}
+
+	const cars = [
+		<Car {...testCar2} />,
+		<Car {...testCar} />,
+		<Car {...testCar2} />,
+		<Car {...testCar} />,
+		<Car {...testCar2} />,
+	]
+	const numberOfDots = isMobile ? cars.length : Math.ceil(cars.length / 3)
 	return (
 		<TopCarsContainer>
 			<Title>Explore Our Top Deals</Title>
 			<CarsContainer>
-				<Carousel></Carousel>
-				<Car {...testCar} />
-				<Car {...testCar} />
-				<Car {...testCar2} />
+				<Carousel
+					value={current}
+					onChange={setCurrent}
+					slides={cars}
+					plugins={[
+						'clickToChange',
+						{
+							resolve: slidesToShowPlugin,
+							options: {
+								numberOfSlides: 3,
+							},
+						},
+					]}
+					breakpoints={{
+						640: {
+							plugins: [
+								{
+									resolve: slidesToShowPlugin,
+									options: {
+										numberOfSlides: 1,
+									},
+								},
+							],
+						},
+						900: {
+							plugins: [
+								{
+									resolve: slidesToShowPlugin,
+									options: {
+										numberOfSlides: 2,
+									},
+								},
+							],
+						},
+					}}
+				/>
+				<Dots value={current} onChange={setCurrent} number={numberOfDots} />
 			</CarsContainer>
 		</TopCarsContainer>
 	)
